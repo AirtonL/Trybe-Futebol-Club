2a2e9f77b217fa7dea61c2a1129d29dd8d698a34
Airtonl <junynnhoh@gmail.com>
2022-05-30 18:41:46 -0300

    feat: create token generate

--- app/backend/src/helpers/jsonWebToken/TokenGenerate.ts
+++ app/backend/src/helpers/jsonWebToken/TokenGenerate.ts
@@ -1,5 +1,6 @@
 import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
+import IPayload from '../../interfaces/IPayload';
 
 const SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8');
 
@@ -8,6 +9,6 @@ const jwtConfig: jwt.SignOptions = {
   expiresIn: '10d',
 };
 
-export const jwtSign = (payload: any) => jwt.sign(payload, SECRET, jwtConfig);
+export const jwtSign = (payload: IPayload) => jwt.sign(payload, SECRET, jwtConfig);
 
 export const jwtVerify = (token:string) => jwt.verify(token, SECRET);
