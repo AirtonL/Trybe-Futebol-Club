bfec63cb966817bff7f43f3e04243179fbc4510e
Airtonl <junynnhoh@gmail.com>
2022-05-30 22:13:10 -0300

    feat: add tests teams

---
+++ app/backend/src/tests/teams.me.test.ts
@@ -0,0 +1,43 @@
+// import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { Response } from 'superagent';
+import { app } from '../app';
+import { allTeams } from './mocksTeams';
+// import Example from '../database/models/ExampleModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes Teams', () => {
+  let chaiHttpResponse: Response;
+  it('verifica se trás todos os teams', async () => {
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/teams');
+
+    expect(chaiHttpResponse.body).to.deep.be.equal(allTeams);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+
+  it('Testa rota pelo ID', async () => {
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/teams/1');
+
+    expect(chaiHttpResponse.body).to.be.deep.equal(allTeams[0]);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+
+  it('Quando envia um id inexistente', async () => {
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/teams/null');
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Server error' });
+    expect(chaiHttpResponse.status).to.be.equal(500);
+  });
+
+});
