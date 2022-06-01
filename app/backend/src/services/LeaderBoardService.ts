973fbb9dc43a30c384e1b2e70c0c57ef55121406
Airtonl <junynnhoh@gmail.com>
2022-06-01 15:00:17 -0300

    refactor: use sortArray npm to logic sortBoard

--- app/backend/src/services/LeaderBoardService.ts
+++ app/backend/src/services/LeaderBoardService.ts
@@ -1,31 +1,18 @@
-import Match from '../database/models/Match';
+import sortArray = require('sort-array');
 import Team from '../database/models/Team';
 import LeaderBoardHelper from '../utils/LeaderBoard';
 
 class LeaderBoardService {
   constructor(
     private teamModel = Team,
-    private matchModel = Match,
   ) {}
 
   async leaderBoard() {
     const teams = await this.teamModel.findAll();
     const table = await LeaderBoardHelper.constructorLeaderBoard(teams);
-    return table.sort((a, b) => {
-      if (a.totalPoints < b.totalPoints) return 1;
-      if (a.totalPoints > b.totalPoints) return -1;
-
-      if (a.goalsBalance < b.goalsBalance) return 1;
-      if (a.goalsBalance > b.goalsBalance) return -1;
-
-      if (a.goalsFavor < b.goalsFavor) return 1;
-      if (a.goalsFavor > b.goalsFavor) return -1;
-
-      if (a.goalsOwn < b.goalsOwn) return -1;
-      if (a.goalsOwn > b.goalsOwn) return 1;
-
-      return 0;
-    });
+    const keys = ['totalPoints', 'totalVictories', 'goalsBalance', 'goalsFavor', 'goalsOwn'];
+    const sort = ['desc', 'desc', 'desc', 'desc', 'desc'];
+    return sortArray(table, { by: keys, order: sort });
   }
 }
 
