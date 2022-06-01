3563baf2a0283fabc0fc631bbec96d0a397a5f07
Airtonl <junynnhoh@gmail.com>
2022-06-01 17:51:22 -0300

    feat: create interface Table

---
+++ app/backend/src/interfaces/ITableService.ts
@@ -0,0 +1,11 @@
+export default interface ITableService {
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  totalPoints: number;
+  goalsBalance: number;
+  efficiency: number;
+}
