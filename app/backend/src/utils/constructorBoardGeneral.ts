d37c8f6ec58a93b8ed5e30c24f5b8960ac1babe0
Airtonl <junynnhoh@gmail.com>
2022-06-03 15:15:25 -0300

    fix: test req33

--- app/backend/src/utils/constructorBoardGeneral.ts
+++ app/backend/src/utils/constructorBoardGeneral.ts
@@ -1,16 +1,21 @@
 import ITableService from '../interfaces/ITableService';
 
-const constructorBoardGeneral = (awayTeam: ITableService, teamHome: ITableService) => ({
-  name: awayTeam!.name,
-  totalPoints: awayTeam!.totalPoints + teamHome.totalPoints,
-  totalGames: awayTeam!.totalGames + teamHome.totalGames,
-  totalVictories: awayTeam!.totalVictories + teamHome.totalVictories,
-  totalDraws: awayTeam!.totalDraws + teamHome.totalDraws,
-  totalLosses: awayTeam!.totalLosses + teamHome.totalLosses,
-  goalsFavor: awayTeam!.goalsFavor + teamHome.goalsFavor,
-  goalsOwn: awayTeam!.goalsOwn + teamHome.goalsOwn,
-  goalsBalance: awayTeam!.goalsBalance + teamHome.goalsBalance,
-  efficiency: +((awayTeam!.efficiency + teamHome.efficiency) / 2).toFixed(2),
-});
+const constructorBoardGeneral = (awayTeam: ITableService, teamHome: ITableService) => {
+  const totalGames = awayTeam!.totalGames + teamHome.totalGames;
+  const totalPoints = awayTeam!.totalPoints + teamHome.totalPoints;
+
+  return {
+    name: awayTeam!.name,
+    totalPoints,
+    totalGames,
+    totalVictories: awayTeam!.totalVictories + teamHome.totalVictories,
+    totalDraws: awayTeam!.totalDraws + teamHome.totalDraws,
+    totalLosses: awayTeam!.totalLosses + teamHome.totalLosses,
+    goalsFavor: awayTeam!.goalsFavor + teamHome.goalsFavor,
+    goalsOwn: awayTeam!.goalsOwn + teamHome.goalsOwn,
+    goalsBalance: awayTeam!.goalsBalance + teamHome.goalsBalance,
+    efficiency: +((totalPoints / (totalGames * 3)) * 100).toFixed(2),
+  };
+};
 
 export default constructorBoardGeneral;
